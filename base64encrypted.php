77fcaf459ebb1f1814c5b26210126a4f116dde1f
base64encrypted <jadnet1@gmx.com>
2017-07-17 20:57:11 +0200

    negative value returned in php modulo fixed

--- base64encrypted.php
+++ base64encrypted.php
@@ -13,31 +13,29 @@ public static function Crypter($a,$b,$xx=4){
 if($a==""||$b==""||!is_int($xx))return $a;
 $e=self::$clef;
 $u="";
-$lb=self::Unorder($e,mt_rand(0,2147483647).$b,64);
+$lb=self::Unorder($e,mt_rand(0,2147483647).$b);
 $oo=chr(mt_rand(0,255));
-for($i=0;$i<$xx;$i++){
-$fd=$lb{(int)self::Seed(63,$oo)};
-$oo=$fd^$oo;$u.=$fd;}
+for($i=0;$i<$xx;$i++){$fd=$lb{(int)self::Seed(63,$oo)};$oo^=$fd;$u.=$fd;}
 $f=md5($b.$u);
-$l=self::Unorder($e,$f,64);
+$l=self::Unorder($e,$f);
 $t=$g="";
 $c=strlen($a);
 $s=$c-$c%3;
 $mi=hexdec(substr($f,-8))&2147483647;
 $n=$mi%64;
 for($r=$i=0;$i<$s;$i+=3,$r++){
-$nr=($n+$r)%2147483642;
-$g=(ord($a{$i}^$l{($nr+1)%64})<<16)+(ord($a{$i+1}^$l{($nr+2)%64})<<8)+(ord($a{$i+2}^$l{($nr+3)%64}));
+$n=fmod($n+=$r,2147483642);
+$g=(ord($a{$i}^$l{($n+1)%64})<<16)+(ord($a{$i+1}^$l{($n+2)%64})<<8)+(ord($a{$i+2}^$l{($n+3)%64}));
 $t.=$l{$g>>18}.$l{($g>>12)&63}.$l{($g>>6)&63}.$l{$g&63};
-srand($mi);$l=str_shuffle($l); /* OR $l=self::Unorder($l,$f,64); but slow without native function... */
+srand($mi);$l=str_shuffle($l); /* OR $l=self::Unorder($l,$f); but slow without native function... */
 }
 switch($c-$s){
 case 1:
-$g=ord($a{$i}^$l{($nr+4)%64})<<16;
+$g=ord($a{$i}^$l{($n+4)%64})<<16;
 $t.=$l{$g>>18}.$l{($g>>12)&63};
 break;
 case 2:
-$g=(ord($a{$i}^$l{($nr+4)%64})<<16)+(ord($a{$i+1}^$l{($nr+5)%64})<<8);
+$g=(ord($a{$i}^$l{($n+4)%64})<<16)+(ord($a{$i+1}^$l{($n+5)%64})<<8);
 $t.=$l{$g>>18}.$l{($g>>12)&63}.$l{($g>>6)&63};
 break;}
 $c=strlen($t);
@@ -57,30 +55,30 @@ $e=self::$clef;
 $ff=md5($b.$u);
 $mi=hexdec(substr($ff,-8))&2147483647;
 $m=$mi%64;
-$l=self::Unorder($e,$ff,64);
+$l=self::Unorder($e,$ff);
 $d=$g="";
 $f=0;
 while($c%4!==0){$a.="=";$c=strlen($a);$c=$c-4;$f++;}
 for($r=$i=0;$i<$c;$i+=4,$r++){
 $q=$e{strpos($l,$a{$i})}.$e{strpos($l,$a{$i+1})}.$e{strpos($l,$a{$i+2})}.$e{strpos($l,$a{$i+3})};
 $g=(strpos($e,$q{0})<<18)+(strpos($e,$q{1})<<12)+(strpos($e,$q{2})<<6)+(strpos($e,$q{3}));
-$nr=($m+$r)%2147483642;
-$d.=(chr($g>>16)^$l{($nr+1)%64}).(chr(($g>>8)&255)^$l{($nr+2)%64}).(chr($g&255)^$l{($nr+3)%64});
-srand($mi);$l=str_shuffle($l); /* OR $l=self::Unorder($l,$ff,64); but slow without native function... */
+$m=fmod($m+=$r,2147483642);
+$d.=(chr($g>>16)^$l{($m+1)%64}).(chr(($g>>8)&255)^$l{($m+2)%64}).(chr($g&255)^$l{($m+3)%64});
+srand($mi);$l=str_shuffle($l); /* OR $l=self::Unorder($l,$ff); but slow without native function... */
 }
 switch($f){
 case 1:
 $q=$e{strpos($l,$a{$i})}.$e{strpos($l,$a{$i+1})}.$e{strpos($l,$a{$i+2})};
 $g=(strpos($e,$q{0})<<18)+(strpos($e,$q{1})<<12)+(strpos($e,$q{2})<<6);
-$d.=(chr($g>>16)^$l{($nr+4)%64}).(chr(($g>>8)&255)^$l{($nr+5)%64});
+$d.=(chr($g>>16)^$l{($m+4)%64}).(chr(($g>>8)&255)^$l{($m+5)%64});
 break;
 case 2:
 $q=$e{strpos($l,$a{$i})}.$e{strpos($l,$a{$i+1})};
 $g=(strpos($e,$q{0})<<18)+(strpos($e,$q{1})<<12);
-$d.=(chr($g>>16)^$l{($nr+4)%64});
+$d.=(chr($g>>16)^$l{($m+4)%64});
 break;}
 return $d;}
-private static function Unorder($x,$b,$c){
+private static function Unorder($x,$b,$c=64){
 $w=0;$y=strlen($b);
 for($i=0;$i<$c;$i++){
 $w=($w+ord($x[$i])+ord($b[$i%$y]))%$c;
@@ -90,4 +88,5 @@ private static function Seed($b,$c){
 $d=unpack("Na",hash("crc32",$c,true));
 return round((($d['a']&2147483647)/2147483647.0)*$b);}
 }
+
 ?>
