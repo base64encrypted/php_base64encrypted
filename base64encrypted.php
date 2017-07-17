d72ca43d9644057069f4df8cc69c2e3ca659601a
base64encrypted <jadnet1@gmx.com>
2017-07-17 21:52:13 +0200

    Update base64encrypted.php

--- base64encrypted.php
+++ base64encrypted.php
@@ -24,18 +24,18 @@ $s=$c-$c%3;
 $mi=hexdec(substr($f,-8))&2147483647;
 $n=$mi%64;
 for($r=$i=0;$i<$s;$i+=3,$r++){
-$r=fmod($r+=$n,2147483642);
-$g=(ord($a{$i}^$l{($r+1)%64})<<16)+(ord($a{$i+1}^$l{($r+2)%64})<<8)+(ord($a{$i+2}^$l{($r+3)%64}));
+$r=(int)fmod($r+=$n,59);
+$g=(ord($a{$i}^$l{$r+1})<<16)+(ord($a{$i+1}^$l{$r+2})<<8)+(ord($a{$i+2}^$l{$r+3}));
 $t.=$l{$g>>18}.$l{($g>>12)&63}.$l{($g>>6)&63}.$l{$g&63};
 srand($mi);$l=str_shuffle($l); /* OR $l=self::Unorder($l,$f); but slow without native function... */
 }
 switch($c-$s){
 case 1:
-$g=ord($a{$i}^$l{($r+4)%64})<<16;
+$g=ord($a{$i}^$l{$r+4})<<16;
 $t.=$l{$g>>18}.$l{($g>>12)&63};
 break;
 case 2:
-$g=(ord($a{$i}^$l{($r+4)%64})<<16)+(ord($a{$i+1}^$l{($r+5)%64})<<8);
+$g=(ord($a{$i}^$l{$r+4})<<16)+(ord($a{$i+1}^$l{$r+5})<<8);
 $t.=$l{$g>>18}.$l{($g>>12)&63}.$l{($g>>6)&63};
 break;}
 $c=strlen($t);
@@ -62,20 +62,20 @@ while($c%4!==0){$a.="=";$c=strlen($a);$c=$c-4;$f++;}
 for($r=$i=0;$i<$c;$i+=4,$r++){
 $q=$e{strpos($l,$a{$i})}.$e{strpos($l,$a{$i+1})}.$e{strpos($l,$a{$i+2})}.$e{strpos($l,$a{$i+3})};
 $g=(strpos($e,$q{0})<<18)+(strpos($e,$q{1})<<12)+(strpos($e,$q{2})<<6)+(strpos($e,$q{3}));
-$r=fmod($r+=$n,2147483642);
-$d.=(chr($g>>16)^$l{($r+1)%64}).(chr(($g>>8)&255)^$l{($r+2)%64}).(chr($g&255)^$l{($r+3)%64});
+$r=(int)fmod($r+=$n,59);
+$d.=(chr($g>>16)^$l{$r+1}).(chr(($g>>8)&255)^$l{$r+2}).(chr($g&255)^$l{$r+3});
 srand($mi);$l=str_shuffle($l); /* OR $l=self::Unorder($l,$ff); but slow without native function... */
 }
 switch($f){
 case 1:
 $q=$e{strpos($l,$a{$i})}.$e{strpos($l,$a{$i+1})}.$e{strpos($l,$a{$i+2})};
 $g=(strpos($e,$q{0})<<18)+(strpos($e,$q{1})<<12)+(strpos($e,$q{2})<<6);
-$d.=(chr($g>>16)^$l{($r+4)%64}).(chr(($g>>8)&255)^$l{($r+5)%64});
+$d.=(chr($g>>16)^$l{$r+4}).(chr(($g>>8)&255)^$l{$r+5});
 break;
 case 2:
 $q=$e{strpos($l,$a{$i})}.$e{strpos($l,$a{$i+1})};
 $g=(strpos($e,$q{0})<<18)+(strpos($e,$q{1})<<12);
-$d.=(chr($g>>16)^$l{($r+4)%64});
+$d.=(chr($g>>16)^$l{$r+4});
 break;}
 return $d;}
 private static function Unorder($x,$b,$c=64){
