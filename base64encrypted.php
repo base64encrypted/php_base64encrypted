9eda8154381885adf9172b933048fa11f3116ced
base64encrypted <jadnet1@gmx.com>
2017-07-31 21:08:41 +0200

    Update base64encrypted.php

--- base64encrypted.php
+++ base64encrypted.php
@@ -5,23 +5,20 @@ For URL encryption, change the key with this one:
 private static $clef="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
 nota: it is possible to build the key with characters of their choice (key length must be equal to 64), while maintaining base64 encoding. Practice is not it?
-In this case take care to adapt the regex accordingly (see bold line).
+In this case take care to adapt the regex accordingly (see below).
 */
 private static $clef="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-public static function Crypter($a,$b,$d,$yy=false,$mda="",$mdb="",$xx=true){
-if($a==""||$b==""||$d==""||!is_bool($yy)||!is_bool($xx))return $a;
-$u=$xx?self::Uranc($b,$d,$mda,$mdb):array("","");
+public static function Crypter($a,$b,$d,$mda,$mdb,$yy=false,$xx=true){
+if($a==""||$b==""||$d==""||$mda==""||$mdb==""||!is_bool($yy)||!is_bool($xx))return $a;
+$u=$xx?self::Uranc($b,$d,$mda):array("","");
 $c=strlen($a);
 if($yy){
-$er=self::Switchkey($b,$d,$mda,$mdb,$u[1]);
-$a=substr_replace($a,substr(md5($a.$u[1],true),self::Seed(10,$er.$u[1]),6),self::Seed($c,$u[1].$er),0);
+$a=substr_replace($a,substr(md5($a.$u[1],true),self::Seed(10,$mdb.$u[1]),6),self::Seed($c,$u[1].$mdb),0);
 $c+=6;}
 $e=self::$clef;
-$ida=md5($b.$u[1],true);
-$l=self::Unorder($e,$ida);
-$idb=md5($d.$u[1],true);
-$li=self::Unorder(range(0,255),$idb,256);
-$ju=md5($mda!=""?$mda.$u[1]:$ida.$idb);$n=hexdec(substr($ju,-8))&255;$ja=md5($mdb!=""?$mdb.$u[1]:$ju);$nb=hexdec(substr($ja,-8))&255;$na=hexdec(substr(md5($ja),-8))&63;
+$l=self::Unorder($e,md5($b.$u[1],true));
+$li=self::Unorder(range(0,255),md5($d.$u[1],true),256);
+$ju=md5($mda.$u[1]);$n=hexdec(substr($ju,-8));$ja=md5($ju);$nb=hexdec(substr($ja,-8));$na=hexdec(substr(md5($ja),-8));
 $s=$c-$c%3;$t=$g="";
 for($n=$n,$na=$na,$nb=$nb,$i=0;$i<$s;$i+=3,$n++,$na++,$nb++){
 $n=(int)fmod($n,256);$nb=(int)fmod($nb,256);$na=(int)fmod($na,64);
@@ -47,28 +44,26 @@ $t.=$l{$hf};$iq=$l{$hf};$l{$hf}=$l{$na=$na++&63};$l{$na}=$iq;
 $t.=$l{(($g>>6)+($li{$nb++&255}))&63};
 break;}
 $c=strlen($t);
-return substr_replace($t,$u[0],self::Seed($c,self::Switchkey($b,$d,$mda,$mdb,$c).$c),0);}
-public static function Decrypter($a,$b,$d,$yy=false,$mda="",$mdb="",$xx=true){
+return substr_replace($t,$u[0],self::Seed($c,$mdb.$c),0);}
+public static function Decrypter($a,$b,$d,$mda,$mdb,$yy=false,$xx=true){
 /*
 For URL encryption, change the regex with this one:
-if(!preg_match("/^[A-z0-9_-]+$/",$a)||$b=="")return $a;
+if(!preg_match("/^[A-z0-9_-]+$/",$a)||$b==""||$d==""||$mda==""||$mdb==""||!is_bool($yy)||!is_bool($xx))return $a;
 */
-if(!preg_match("/^[A-z0-9\/+]+$/",$a)||$b==""||$d==""||!is_bool($yy)||!is_bool($xx))return $a;
+if(!preg_match("/^[A-z0-9\/+]+$/",$a)||$b==""||$d==""||$mda==""||$mdb==""||!is_bool($yy)||!is_bool($xx))return $a;
 $c=strlen($a);
 $da=$g=$u="";
 if($xx){
 $c-=8;
-$mm=self::Seed($c,self::Switchkey($b,$d,$mda,$mdb,$c).$c);
+$mm=self::Seed($c,$mdb.$c);
 $u=substr($a,$mm,8);
 $pr=substr($a,-($c-$mm));
 $a=substr($a,0,$mm).(strlen($pr)==$c+8?"":$pr);
-$u=Base64_Encrypted::Decrypter($u,$b,$d,false,$mda,$mdb,false);}
+$u=Base64_Encrypted::Decrypter($u,$b,$d,$mda," ",false,false);}
 $e=self::$clef;
-$ida=md5($b.$u,true);
-$l=self::Unorder($e,$ida);
-$idb=md5($d.$u,true);
-$li=self::Unorder(range(0,255),$idb,256);
-$ju=md5($mda!=""?$mda.$u:$ida.$idb);$n=hexdec(substr($ju,-8))&255;$ja=md5($mdb!=""?$mdb.$u:$ju);$nb=hexdec(substr($ja,-8))&255;$na=hexdec(substr(md5($ja),-8))&63;
+$l=self::Unorder($e,md5($b.$u,true));
+$li=self::Unorder(range(0,255),md5($d.$u,true),256);
+$ju=md5($mda.$u);$n=hexdec(substr($ju,-8));$ja=md5($ju);$nb=hexdec(substr($ja,-8));$na=hexdec(substr(md5($ja),-8));
 $f=0;
 while($c%4!==0){$a.="=";$c=strlen($a);$c=$c-4;$f++;}
 for($n=$n,$na=$na,$nb=$nb,$i=0;$i<$c;$i+=4,$n++,$na++,$nb++){
@@ -96,21 +91,15 @@ break;}
 if($yy){
 $pj=strlen($da);
 $c=$pj-6;
-$er=self::Switchkey($b,$d,$mda,$mdb,$u);
-$mm=self::Seed($c,$u.$er);
+$mm=self::Seed($c,$u.$mdb);
 $pr=substr($da,-($c-$mm));
 $a=substr($da,0,$mm).(strlen($pr)==$pj?"":$pr);
-$da=substr(md5($a.$u,true),self::Seed(10,$er.$u),6)!=substr($da,$mm,6)?die("Corrupted data !"):$a;}
+$da=substr(md5($a.$u,true),self::Seed(10,$mdb.$u),6)!=substr($da,$mm,6)?die("Corrupted data !"):$a;}
 return $da;}
-private static function Switchkey($b,$d,$mda,$mdb,$c){
-$gk=3;$dq=array(0=>$b,1=>$d);
-$mda!=""?array_push($dq,$mda):$gk--;
-$mdb!=""?array_push($dq,$mdb):$gk--;
-return $dq[self::Seed($gk,($mda!=""?$mda:($mdb!=""?$mdb:$d)).$c)];}
-private static function Uranc($b,$d,$mda,$mdb){
+private static function Uranc($b,$d,$mda){
 $u="";$oo=mt_rand(0,1073741823);
 for($i=1;$i<7;$i++){$fd=chr((int)self::Seed(255,$oo));$oo=fmod($oo+=ord($fd)+$i+mt_rand(0,1073741569-(ord($fd)+$i)),1073741824);$u.=$fd;}
-return array(Base64_Encrypted::Crypter($u,$b,$d,false,$mda,$mdb,false),$u);}
+return array(Base64_Encrypted::Crypter($u,$b,$d,$mda," ",false,false),$u);}
 private static function Unorder($x,$b,$c=64){
 $w=0;$y=strlen($b);
 for($i=0;$i<$c;$i++){$w=($w+ord($x{$i})+ord($b{$i%$y}))%$c;$j=$x{$i};$x{$i}=$x{$w};$x{$w}=$j;}
@@ -118,5 +107,4 @@ return $x;}
 private static function Seed($b,$c){
 return round(((hexdec(substr(md5($c),-8))&2147483647)/2147483647.0)*$b);} 
 }
-
 ?>
