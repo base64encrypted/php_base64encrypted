31a1481a5d7f5c8df712fc922bcbfec272ada588
base64encrypted <jadnet1@gmx.com>
2017-08-04 17:36:36 +0200

    Update base64encrypted.php

--- base64encrypted.php
+++ base64encrypted.php
@@ -5,20 +5,18 @@ For URL encryption, change the key with this one:
 private static $clef="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
 
 nota: it is possible to build the key with characters of their choice (key length must be equal to 64), while maintaining base64 encoding. Practice is not it?
-In this case take care to adapt the regex accordingly (see below).
+In this case take care to adapt the regex accordingly (see bold line).
 */
 private static $clef="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 public static function Crypter($a,$b,$d,$mda,$mdb,$yy=false,$xx=true){
 if($a==""||$b==""||$d==""||$mda==""||$mdb==""||!is_bool($yy)||!is_bool($xx))return $a;
 $u=$xx?self::Uranc($b,$d,$mda):array("","");
 $c=strlen($a);
-if($yy){
-$a=substr_replace($a,substr(md5($a.$u[1],true),self::Seed(10,$mdb.$u[1]),6),self::Seed($c,$u[1].$mdb),0);
-$c+=6;}
+if($yy){$a=substr_replace($a,substr(hash("md4",$a.$u[1],true),self::Seed(10,$mdb.$u[1]),6),self::Seed($c,$u[1].$mdb),0);$c+=6;}
 $e=self::$clef;
-$l=self::Unorder($e,md5($b.$u[1],true));
-$li=self::Unorder(range(0,255),md5($d.$u[1],true),256);
-$ju=md5($mda.$u[1]);$n=hexdec(substr($ju,-8));$ja=md5($ju);$nb=hexdec(substr($ja,-8));$na=hexdec(substr(md5($ja),-8));
+$l=self::Unorder($e,self::keylen($u[1].$b,3));
+$li=self::Unorder(range(0,255),self::keylen($u[1].$d,15),256);
+$n=self::Hashich($mda.$u[1]);$nb=self::Hashich($n);$na=self::Hashich($nb);
 $s=$c-$c%3;$t=$g="";
 for($n=$n,$na=$na,$nb=$nb,$i=0;$i<$s;$i+=3,$n++,$na++,$nb++){
 $n=(int)fmod($n,256);$nb=(int)fmod($nb,256);$na=(int)fmod($na,64);
@@ -61,9 +59,9 @@ $pr=substr($a,-($c-$mm));
 $a=substr($a,0,$mm).(strlen($pr)==$c+8?"":$pr);
 $u=Base64_Encrypted::Decrypter($u,$b,$d,$mda," ",false,false);}
 $e=self::$clef;
-$l=self::Unorder($e,md5($b.$u,true));
-$li=self::Unorder(range(0,255),md5($d.$u,true),256);
-$ju=md5($mda.$u);$n=hexdec(substr($ju,-8));$ja=md5($ju);$nb=hexdec(substr($ja,-8));$na=hexdec(substr(md5($ja),-8));
+$l=self::Unorder($e,self::keylen($u.$b,3));
+$li=self::Unorder(range(0,255),self::keylen($u.$d,15),256);
+$n=self::Hashich($mda.$u);$nb=self::Hashich($n);$na=self::Hashich($nb);
 $f=0;
 while($c%4!==0){$a.="=";$c=strlen($a);$c=$c-4;$f++;}
 for($n=$n,$na=$na,$nb=$nb,$i=0;$i<$c;$i+=4,$n++,$na++,$nb++){
@@ -93,17 +91,26 @@ $c=strlen($da)-6;
 $mm=self::Seed($c,$u.$mdb);
 $pr=substr($da,-($c-$mm));
 $a=substr($da,0,$mm).(strlen($pr)==$c+6?"":$pr);
-$da=substr(md5($a.$u,true),self::Seed(10,$mdb.$u),6)!=substr($da,$mm,6)?die("Corrupted data !"):$a;}
+$da=substr(hash("md4",$a.$u,true),self::Seed(10,$mdb.$u),6)!=substr($da,$mm,6)?die("Corrupted data !"):$a;}
 return $da;}
 private static function Uranc($b,$d,$mda){
-$u="";$oo=mt_rand(0,1073741823);
-for($i=1;$i<7;$i++){$fd=chr((int)self::Seed(255,$oo));$oo=fmod($oo+=ord($fd)+$i+mt_rand(0,1073741569-(ord($fd)+$i)),1073741824);$u.=$fd;}
+$u="";$oo=mt_rand(0,255);
+for($i=1;$i<7;$i++){$fd=chr($oo);$q=2147483647-($oo+$i);$oo=self::Hashich(fmod($oo+=$oo+$i+mt_rand(0,$q),$q))&255;$u.=$fd;}
 return array(Base64_Encrypted::Crypter($u,$b,$d,$mda," ",false,false),$u);}
+private static function Hashich($ke){
+$ke=(string)$ke;
+$a=0;
+$l=strlen($ke);
+for($i=0;$i<$l;$i++){$h=$a<<5;$h-=$a;$h+=ord($ke{$i});$a=$h;$a&=4294967295;}
+return $a;}
+private static function keylen($a,$b){
+$y=hash("md4",$a,true);
+for($i=0;$i<$b;$i++){$y.=hash("md4",$y,true);}
+return $y;}
 private static function Unorder($x,$b,$c=64){
-$w=0;$y=strlen($b);
+$w=0;$y=strlen($b);$t=$b;
 for($i=0;$i<$c;$i++){$w=($w+ord($x{$i})+ord($b{$i%$y}))%$c;$j=$x{$i};$x{$i}=$x{$w};$x{$w}=$j;}
 return $x;}
-private static function Seed($b,$c){
-return round(((hexdec(substr(md5($c),-8))&2147483647)/2147483647.0)*$b);} 
+private static function Seed($b,$c){return round((self::Hashich($c)&2147483647/2147483647.0)*$b);} 
 }
 ?>
