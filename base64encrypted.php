251114a585c8620d702a19bbc82eda7f873d221e
base64encrypted <jadnet1@gmx.com>
2017-08-07 11:15:49 +0200

    Update base64encrypted.php

--- base64encrypted.php
+++ base64encrypted.php
@@ -14,9 +14,9 @@ $u=$xx?self::Uranc($b,$mda):array("","");
 $c=strlen($a);
 if($yy){$a=substr_replace($a,substr(hash("md4",$u[1].$a,true),self::Seed(10,$u[0].$mdb),6),self::Seed($c,$u[1].$mdb),0);$c+=6;}
 $l=self::Unorder(self::$clef,self::Hashich($u[1].substr($b,0,58),64,true));
-$n=self::Hashich($u[1].$mda);$j=self::Hashich($n);$na=self::Hashich($j)&63;
+$n=self::Hashich($u[1].$mda);$j=self::Hashich($n);$na=self::Hashich($j);
 $s=$c-$c%3;$t=$g="";
-for($na=$na,$i=0;$i<$s;$i+=3,$na++&63){
+for($na=$na,$i=0;$i<$s;$i+=3,$na++){
 $n=$h=(($n<<5)-$n)%2147483647;$g=(ord($a{$i}^chr($h))<<16);
 $n=$h=(($n<<5)-$n)%2147483647;$g+=(ord($a{$i+1}^chr($h))<<8);
 $n=$h=(($n<<5)-$n)%2147483647;$g+=(ord($a{$i+2}^chr($h)));
@@ -63,10 +63,10 @@ $a=substr($a,0,$mm).(strlen($pr)==$c+8?"":$pr);
 $u=Base64_Encrypted::Decrypter($uu,$b,$mda," ",false,false);}
 $e=self::$clef;
 $l=self::Unorder($e,self::Hashich($u.substr($b,0,58),64,true));
-$n=self::Hashich($u.$mda);$j=self::Hashich($n);$na=self::Hashich($j)&63;
+$n=self::Hashich($u.$mda);$j=self::Hashich($n);$na=self::Hashich($j);
 $f=0;
 while($c%4!==0){$a.="=";$c=strlen($a);$c=$c-4;$f++;}
-for($na=$na,$i=0;$i<$c;$i+=4,$na++&63){
+for($na=$na,$i=0;$i<$c;$i+=4,$na++){
 $ha=strpos($l,$a{$i});$iq=$l{$ha};$l{$ha}=$l{$na=$na++&63};$l{$na}=$iq;
 $hb=strpos($l,$a{$i+1});$iq=$l{$hb};$l{$hb}=$l{$na=$na++&63};$l{$na}=$iq;
 $hc=strpos($l,$a{$i+2});$iq=$l{$hc};$l{$hc}=$l{$na=$na++&63};$l{$na}=$iq;
